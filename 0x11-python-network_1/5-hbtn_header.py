9b7854685d793dd6cefd10854eb7eb19dce4febd
Alcatraz1221 <111422710+Gonfrx@users.noreply.github.com>
2023-12-05 08:22:17 +0000

    Update 5-hbtn_header.py

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
-"""Fetches a url and displays value of a variable in the response header"""
+"""
+sends a request to the URL passed as an arrgument and
+displays the value of the variable X-Request-Id in the
+response header using requests package
+"""
 
-if __name__ == "__main__":
-    import requests
-    import sys
+import requests
+import sys
+
+if __name__ == '__main__':
     url = sys.argv[1]
-    response = requests.get(url)
-    x_request_id = response.headers.get('X-Request-Id')
-    print(x_request_id)
-
+    res = requests.get(url)
+    print(res.headers.get('X-Request-Id'))
